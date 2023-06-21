27d4409662f99fab49078eae584be63987c12271
Ruslan Sharifullin <newwavecfy@gmail.com>
2023-06-21 13:37:49 +0300

    changed responsive layout logic

--- src/shared/progress_bar/ProgressBar.tsx
+++ src/shared/progress_bar/ProgressBar.tsx
@@ -1,5 +1,5 @@
-import { getMatchingPage } from '../../assets/const/page_paths'
 import { dotConfigurations } from '../../assets/const/progressBar_dots'
+import { getMatchingPage } from '../../assets/const/page_paths'
 import './progressBar.scss'
 
 const ProgressBar: React.FC = () => {
@@ -8,23 +8,25 @@ const ProgressBar: React.FC = () => {
   return (
     <>
       {matchingPage && (
-        <div className="progress-bar">
-          <div
-            className="progress"
-            style={{ width: `${matchingPage.percentage}%` }}
-          />
+        <div className="line">
+          <div className="progress-bar">
+            <div
+              className="progress"
+              style={{ width: `${matchingPage.percentage}%` }}
+            />
 
-          <div className="levels">
-            <div className="dots">
-              {dotConfigurations[matchingPage.label].map((dot, index) => (
-                <img key={index} src={dot} alt={`dot_${index}`} />
-              ))}
-            </div>
+            <div className="levels">
+              <div className="dots">
+                {dotConfigurations[matchingPage.label].map((dot, index) => (
+                  <img key={index} src={dot} alt={`dot_${index}`} />
+                ))}
+              </div>
 
-            <div className="numbers">
-              <p>1</p>
-              <p>2</p>
-              <p>3</p>
+              <div className="numbers">
+                <p>1</p>
+                <p>2</p>
+                <p>3</p>
+              </div>
             </div>
           </div>
         </div>
